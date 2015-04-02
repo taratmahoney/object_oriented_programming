bf01986f8f80a34adcd75806f1dfa0dfeb438106
Tara Mahoney <tara.mahoney@me.com>
2015-04-02 16:45:03 -0400

    Created instances of student & instructor & printed their names.

--- people.rb
+++ people.rb
@@ -1,5 +1,31 @@
-class Student
+class Person
+	def initialize(name)
+		@name = name
+	end
+
+	def print_name	
+		puts "Hi! My name is #{@name}!"
+	end
 end
 
-class Instructor
+
+class Student < Person
+ 	def learn
+ 		puts "I get it!"
+ 	end
 end
+
+class Instructor < Person
+ 	def teach
+ 		puts "Everything in Ruby is an Object!"
+ 	end
+end
+
+i = Instructor.new("Chris")
+i.print_name
+
+
+
+
+# Create an instance of an object to save it s = Person.new("")
+
