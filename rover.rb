8b9eed38bb3b9a7e622c41a0ae1da1ad664374ab
Tara Mahoney <tara.mahoney@me.com>
2015-04-05 21:17:09 -0400

    Used Frank Xu's code to help me finish up & understand what was going on. Added ability to take lines of commands (MLLLMMR) and a second rover to chose and command. Also added plateau that rovers had to operate on.

--- rover.rb
+++ rover.rb
@@ -3,13 +3,33 @@
 
 
 
+module RoverProcess
+	def process_commands(commands)
+		commands.split("").each do |command|
+			case command
+				when "M"
+					move_forward
+				when "R"
+					turn_right
+				when "L"
+					turn_left
+			
+			end
+		end
+	end
+end
+
 class Rover
+
+	attr_accessor :x, :y, :direction
+
 	def initialize (init_x, init_y, init_direction) ##State!!
 		@x = init_x
 		@y = init_y
 		@direction = init_direction
 	end
 
+	include RoverProcess
 		#Behavioiur
 	def move_forward
 		if @direction == "N"
@@ -20,6 +40,8 @@ class Rover
 			@y = @y - 1
 		elsif @direction == "W"
 			@x = @x - 1
+		else
+			puts "Error!"	
 		end	
 	end
 	
@@ -36,6 +58,8 @@ class Rover
 
 		elsif @direction == "E"
 			@direction = "N"
+		else 
+			puts "Error!"	
 
 		end
 	end
@@ -50,6 +74,8 @@ class Rover
 			 @direction = "W"
 		elsif @direction == "E"
 			 @direction = "S"	
+		else
+			puts "Error!"
 		end
 	end
 
@@ -73,18 +99,74 @@ class Rover
 	end
 end
 
-my_rover = Rover.new(1, 2, "N") #pass parameters into new 
-my_rover.to_s
-my_rover.read_instruction(my_rover)
-my_rover.to_s
+class Plateau
 
+	def initialize (width, height)
+		@width = width
+		@height = height
+		@rovers = []
+	end	
+
+	include RoverProcess
+
+ 	def ask_for_size
+ 		puts "How big should the plateau be?"
+ 		@size = gets.chomp
+	end	
+	
+	def materialize_rover(x, y, direction)
+		if x > @width or x < 0 or y > @height or y < 0
+			puts "GET YOUR SHIT TOGETHER. THIS IS NOT EVEN ON THE PLATEAU, YO."
+		else
+			new_rover = Rover.new(x, y, direction)
+			@rovers << new_rover
+		end	
+	end	
+
+	def choose_rover
+		puts "Which rover do you want to command?"
+		puts "Enter '1' for Rover 1"
+		puts "Enter '2' for Rover 2"
+		user_input = gets.chomp.to_i
+
+		puts "Where do you want your rover to go?"
+		puts "Write 'L' to turn left"
+		puts "Write 'R' to turn right"
+		puts "Write 'M' to move forward"
+		puts "For example, MLLLMMMM"
+
+		commands = gets.chomp
+		@rovers[user_input - 1].process_commands(commands)
+	end	
+
+	def to_s 
+		"Plateau width: #{@width}, height: #{@height}, Rovers: \n\n#{@rovers.map{|r| r.to_s}}"
+	end	
+
+end		
+
+plateau = Plateau.new(10,10)
+plateau.materialize_rover(1, 2, "N")
+plateau.materialize_rover(3, 3, "E")
+puts plateau
+plateau.choose_rover
+puts plateau
+plateau.choose_rover
+puts plateau
+
+# my_rover = Rover.new(1, 2, "N") #pass parameters into new 
+# my_rover.to_s
+# my_rover.read_instruction(my_rover)
+# my_rover.to_s
+
+## From breakout session
 #puts my_rover 
 #my_rover.move_forward
 #puts my_rover
 #my_rover.turn_left
 #puts my_rover
 
-## Look up the split method
+## Look up the .split method
 
 ## Domain mdoeling
 # 1. What are the things?
